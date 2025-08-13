3e523e1188021c29afff6cb566fe083ae2226926
Kym Michael <kym.michael@gmail.com>
2025-08-13 20:06:24 +0800

    Update i.php

--- i.php
+++ i.php
@@ -10,13 +10,53 @@
     echo $header;
     ?>
     
-   <main>
-        <h1>Welcome</h1>
-        <?php
-        $greeting = "Hello, visitor!";
-        echo "<p>$greeting</p>";
-        ?>
-    </main>
+
+</div>
+
+<div class="box">
+<div class="idx">
+<p><b>
+    <span class="col">b</span> c <s>d</s> e <u>f</u><span class="col"> g
+    </span> <s>h</s><span class="col"> i</span> <u>j</u> o p <s>s</s> r <s>t</s> v x
+    <span class="col"> 7</span>
+</b><br>
+    @sufbo.tplinkdns.com <s>~1Msec</s> <u>no -D forward</u>
+</p>
+</div>
+</div>
+    <div class="bsd">
+<p>
+<i>DE.</i> <b>b</b>sd.tilde.team <span class="sml">157.90.196.52</span> 
+<a href="https://bsd.tilde.team/">b.t.t</a> 
+<a href="https://bsd.tilde.team/~identity2">b.t.t/~i52</a> 
+<p>
+<?php
+    $bbb = file_get_contents('https://bsd.tilde.team/~identity2/a.txt');
+    echo $bbb;
+?>
+</p>
+</div>
+</div>
+
+    
+<div class="box">
+<p>
+<i>US.</i> <b>c</b>trl-c.club <span class="tin">165.227.127.54</span> <i>NewsB</i> 
+<a href="https://ctrl-c.club/">c.c</a>
+<a href="https://ctrl-c.club/~i52">c.c/~i72</a>
+<a href="https://ctrl-c.club/~identity2/lineage/">c.c/~i72/lineage</a> &nbsp; 
+</p>
+<p>
+<?php
+    $ccc = file_get_contents('https://ctrlc-club/~identity2/a.txt');
+    echo $ccc;
+?>
+
+
+</div></p>
+</div>
+
+    
     
     <?php
     // Load footer
