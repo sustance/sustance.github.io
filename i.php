d4932f370ed64b38751341bc9c5cb2a38e7794a3
Kym Michael <kym.michael@gmail.com>
2025-08-13 20:34:41 +0800

    Update i.php

--- i.php
+++ i.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Home Page</title>
-</head>
-<body>
-    <?php 
-    // Load header
+<?php 
     $header = file_get_contents('https://raw.githubusercontent.com/sustance/sustance.github.io/refs/heads/main/head-land.html');
     echo $header;
-    ?>
-    
+?>
 
-</div>
 
 <div class="box">
 <div class="idx">
@@ -24,44 +15,46 @@
 </p>
 </div>
 </div>
-    <div class="bsd">
+
+    
+    
+<div class="bsd">
 <p>
 <i>DE.</i> <b>b</b>sd.tilde.team <span class="sml">157.90.196.52</span> 
 <a href="https://bsd.tilde.team/">b.t.t</a> 
 <a href="https://bsd.tilde.team/~identity2">b.t.t/~i52</a> 
-<p>
+
 <?php
     $bbb = file_get_contents('https://bsd.tilde.team/~identity2/a.txt');
     echo $bbb;
 ?>
 </p>
 </div>
-</div>
+
 
     
 <div class="box">
-<p>
 <i>US.</i> <b>c</b>trl-c.club <span class="tin">165.227.127.54</span> <i>NewsB</i> 
 <a href="https://ctrl-c.club/">c.c</a>
 <a href="https://ctrl-c.club/~i52">c.c/~i72</a>
 <a href="https://ctrl-c.club/~identity2/lineage/">c.c/~i72/lineage</a> &nbsp; 
-</p>
 <p>
 <?php
-    $ccc = file_get_contents('https://ctrlc-club/~identity2/a.txt');
+    $ccc = file_get_contents('https://ctrlc-c.club/~identity2/a.txt');
     echo $ccc;
 ?>
-
-
-</div></p>
+</p>
 </div>
 
+
+
+
     
-    
-    <?php
+<?php
     // Load footer
     $footer = file_get_contents('https://raw.githubusercontent.com/sustance/sustance.github.io/refs/heads/main/tail-land.html');
     echo $footer;
-    ?>
+?>
+
 </body>
 </html>
