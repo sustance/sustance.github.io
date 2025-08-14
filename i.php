48497792ad9840d04850482a4e6fffa7f73fd461
Kym Michael <kym.michael@gmail.com>
2025-08-14 14:59:38 +0800

    Update i.php

--- i.php
+++ i.php
@@ -46,19 +46,6 @@ $ccc = file_get_contents('https://ctrl-c.club/~identity2/a.txt'); echo $ccc;
 <i>GB.</i> <b>d</b>imesion.sh <span class="sml">209.97.187.90</span> 
 <a href="https://dimension.sh/">d.s</a> 
 <a href="https://dimension.sh/~identity2">d.s/~i72</a></p>
-
-<p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]D s1.dimensi Lin                 bash<br>
-!D w3m lynx <s>jek</s> <s>nwsb</s>    <br>
-[D y2:x:1073: 100::/h/y2:/b/bash<br>
-(D 12:05/11 04:05/11 |dn.1   hk.    av.       <br>
-
-<span style='color: var(--col10-7);'>  :11 2:03   2:30 2:52 5:39 3:55 :23  :10          
-</span>
-<a href='#D'> ...show cron</a>
-<div id='D'>1 4 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh <br>
-1 4 * * * ${fr}configs/refs/heads/main/mm -o ${to}mm <br>
-5 4 * * * C_ID=D ${to}do.sh <br>
-</div></p>
 </div>
 
 	<div class="box">
@@ -68,29 +55,6 @@ identity2@linkedin.com aaa John Smith p;;lin <a href="https://mail.envs.net/webm
 <a href="https://envs.net/">e.n</a>  
 <a href="https://envs.net/~identity2">e.n/~i72</a>  
 <a href="https://envs.net/~identity2/lineage/">e.n/~i72/lineage</a> </p>
-
-<p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]E core.envs. Lin 5.2 7.4 2.7 3.2 sh <br>
-!E w3m lynx jek nwsb    <br>
-[E y2:x:10750:9999::/h/y2:/b/bash<br>
-(E 12:05/11 04:05/11 |dn.4   hk.    av.       <br>
-
-<span style='color: var(--col10-7);'>  
-</span>
-<a href='#E'> ...show cron</a>
-<div id='E'>1 4 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh <br>
-1 4 * * * ${fr}configs/refs/heads/main/mm  -o ${to}mm <br>
-5 4 * * * C_ID=E ${to}do.sh <br>
-40  * * * * ${to}newsLatest.sh <br>
-0 */12 * * * ${to}/start-jekyll.sh <br>
-0 0 21 5 * ${to}threedays.sh > /dev/null 2>&1
-0 0 25 5 * ${to}today.sh > /dev/null 2>&1
-0 0 2 7 *  ${to}threedays.sh > /dev/null 2>&1
-0 0 7 7 *  ${to}today.sh > /dev/null 2>&1
-0 0 6 8 *  ${to}threedays.sh > /dev/null 2>&1
-0 0 11 8 * ${to}today.sh > /dev/null 2>&1
-0 0 13 8 * ${to}threedays.sh > /dev/null 2>&1
-0 0 18 8 * ${to}today.sh > /dev/null 2>&1
-</div></p>
 </div>
 
 	<div class="box">
@@ -101,18 +65,6 @@ identity2@linkedin.com aaa John Smith p;;lin <a href="https://mail.envs.net/webm
 <a href="https://freeshell.de/~identiy/lineage/">f.d/~i6y/lineage</a> 
 SHA256:<br> pDFR0atCakVtmZProASRXRGq2aXyKxW6KMx8F6gZtvE (RSA)new<br>
 ssh ssh.freeshell.de -p 443 or -p 32768</p>
-
-<p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]F freeshell. Lin     7.4 2.7 3.1 sh <br>
-!F w3m lynx <s>jek</s> nwsb    <br>
-[F y:x:3334:1000:y:/h/i/y:/b/bash<br>
-(F 12:05/11 06:05/11 |dn.5   hk.    av.       <br>
-
-<span style='color: var(--col10-7);'>no last access info</span>
-<a href='#F'> ...show cron</a>
-<div id='F'>1 6 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh <br>
-1 6 * * * ${fr}configs/refs/heads/main/mm -o ${to}mm <br>
-5 6 * * * C_ID=F ${to}do.sh <br>
-</div></p>
 </div>
 
 	<div class="bsd">
@@ -121,19 +73,6 @@ ssh ssh.freeshell.de -p 443 or -p 32768</p>
 <a href="https://tilde.guru">t.g</a>  
 <a href="https://tilde.guru/~edi">t.g/~edi</a>
 </p>
-
-<p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]G tilde.guru Fre         3.2     sh <br>
-!G w3m lynx <s>jek</s> <s>nwsb</s>    <br>
-[G edi:*:1148:1149::/h/edi:/u/l/b/bash<br>
-(G 12:05/11 04:05/11 |dn.1   hk.    av.       <br>
-
-<span style='color: var(--col10-7);'>  
-</span>
-<a href='#G'> ...show cron</a>
-<div id='G'>1 4 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh <br>
-1 4 * * * ${fr}configs/refs/heads/main/mm -o ${to}mm <br>
-5 4 * * * C_ID=G ${to}do.sh <br>
-</div></p>
 </div>
 
 	<div class="box">
@@ -144,19 +83,6 @@ ssh ssh.freeshell.de -p 443 or -p 32768</p>
 2043108 <a href="https://tilde.tel">t.tel</a><br>
 Uname: 2043108, Pwrd: p;;tel,<br>
 Svr: connect.tilde.tel, Port: 5060, Transp: UDP</p>
-
-<p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]H thunix thu Lin 5.3 8.2 3.1 3.3 sh <br>
-!H w3m lynx jek nwsb    <br>
-[H id:x:1123: 100::/h/id:/u/b/bash<br>
-(H 12:05/11 22:05/10 |dn.2   hk.    av.       <br>
-
-<span style='color: var(--col10-7);'>  
-</span>
-<a href='#H'> ...show cron</a>
-<div id='H'>1 22 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh <br>
-1 22 * * * ${fr}configs/refs/heads/main/mm -o ${to}mm <br>
-5 22 * * * C_ID=H /usr/bin/bash ${to}do.sh <br>
-</div></p>
 </div>
 
 	<div class="bsd">
@@ -164,19 +90,6 @@ Svr: connect.tilde.tel, Port: 5060, Transp: UDP</p>
 <span class="sml">159.69.146.152</span>  
 <a href="https://tilde.institute">t.i</a>  
 <a href="https://id2.tilde.institute">id2.t.i</a> </p>
-
-<p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]I tilde.inst Ope     8.1 3.1     sh <br>
-!I w3m lynx <s>jek</s> nwsb    <br>
-[I id2:*:1804:1001::/h/id2:/u/l/b/bash<br>
-(I 12:05/11 04:05/11 |dn.4   hk.    av.       <br>
-
-<span style='color: var(--col10-7);'>  
-</span>
-<a href='#I'> ...show cron</a>
-<div id='I'>1 4 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh <br>
-1 4 * * * ${fr}configs/refs/heads/main/mm -o ${to}mm <br>
-5 4 * * * C_ID=I ${to}do.sh <br>
-</div></p>
 </div>
 
 	<div class="box">
@@ -187,53 +100,20 @@ Svr: connect.tilde.tel, Port: 5060, Transp: UDP</p>
 <p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]J pubnix pub Lin     8.4         sh <br>
 !J w3m lynx <s>jek</s> <s>nwsb</s>    <br>
 [J y2:x:30183:30184::/h/y2:/b/bash<br>
-(J 12:05/11 04:05/11 |dn.3   hk.    av.       <br>
-
-<span style='color: var(--col10-7);'>  
-</span>
-<a href='#J'> ...show cron</a>
-<div id='J'>1 4 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh <br>
-1 4 * * * ${fr}configs/refs/heads/main/mm -o ${to}mm <br>
-5 4 * * * C_ID=J /home/identity2/.local/bin/do.sh <br>
-</div></p>
+(J 12:05/11 04:05/11 |dn.3   hk.    av.     
+</p>
 </div>
 
 	<div class="box">
 <p><i>DE.</i> pr<b>o</b>jectsegfau.lt <span class="sml">45.145.41.226</span>
 <a href="https://projectsegfau.lt/">p.l</a> 
 <a href="https://id.p.projectsegfau.lt/">i.p.p.l/~id</a></p>
-
-<p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]O pubnix pub Lin     8.4         sh <br>
-!O w3m lynx <s>jek</s> <s>nwsb</s>    <br>
-[O id:x:30337:30338::/h/id:/b/bash<br>
-(O 12:05/11 04:05/11 |dn.4   hk.    av.       <br>
-
-<span style='color: var(--col10-7);'>  
-</span>
-<a href='#O'> ...show cron</a>
-<div id='O'>1 4 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh <br>
-1 4 * * * ${fr}configs/refs/heads/main/mm -o ${to}mm <br>
-5 4 * * * C_ID=O /home/id/.local/bin/do.sh <br>
-</div></p>
 </div>
 
 	<div class="bsd">
 <p><i>CA.</i> &nbsp; tilde.<b>p</b>ink  
 <span class="sml">198.50.210.248 </span> <a href="https://tilde.pink/">t.p</a>  
 <a href="https://tilde.pink/~id2">t.p/~id2</a> </p>
-
-<p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]P rosa.tilde Net 5.4 8.3 3.4 3.6 sh <br>
-!P w3m lynx <s>jek</s> <s>nwsb</s>    <br>
-[P id2:*:1414: 100::/h/id2:/u/p/b/bash<br>
-(P 12:05/11 04:05/11 |dn.    hk.    av.       <br>
-
-<span style='color: var(--col10-7);'>  
-</span>
-<a href='#P'> ...show cron</a>
-<div id='P'>1 4 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh <br>
-1 4 * * * ${fr}configs/refs/heads/main/mm -o ${to}mm <br>
-5 4 * * * C_ID=P ${to}do.sh <br>
-</div></p>
 </div>
 
 	<div class="bsd">
@@ -247,58 +127,18 @@ Svr: connect.tilde.tel, Port: 5060, Transp: UDP</p>
 <p><i>CA.</i> tilde.<b>t</b>eam <span class="sml">198.50.210.248</span> <i>Newsb</i> <a href="https://tilde.team/">t.t</a> 
 <a href="https://tilde.team/~identity2">t.t/~i72</a> 
 <a href="https://tilde.team/~identity2/lineage">t.t/~i72/lineage</a> </p>
-
-<p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]T tilde tild Lin 5.3 8.1 3.0 3.3 sh <br>
-!T w3m lynx jek nwsb    <br>
-[T y2:x:2595: 100::/h/y2:/u/b/bash<br>
-(T 12:05/11 04:05/11 |dn.1   hk.    av.       <br>
-
-<span style='color: var(--col10-7);'>  3:57   
-</span>
-<a href='#T'> ...show cron</a>
-<div id='T'>1 4 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh <br>
-1 4 * * * ${fr}configs/refs/heads/main/mm -o ${o}mm <br>
-1 4 * * * ${fr}configs/refs/heads/main/urls -o ${nb} <br>
-5 4 * * * C_ID=T ${to}do.sh <br>
-</div></p>
 </div>
 
 	<div class="box">
 <p><i>US.</i> <b>v</b>ern.cc <span class="sml">5.161.108.85</span> 
 <a href="https://vern.cc/">v.c</a> 
 <a href="https://id.vern.cc/">id.v.c/~id</a></p>
-
-<p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]V verntil ve Lin 5.2             sh <br>
-!V <s>w3m</s> <s>lynx</s> <s>jek</s> <s>nwsb</s>    <br>
-[V id:x:1391:1394::/h/id:/run/c-s/sw/b/bash<br>
-(V 12:05/11 04:05/11 |dn.    hk.    av.       <br>
-
-<span style='color: var(--col10-7);'>:45  :33 :50 :13 5:39  1:46 :11 :32 :20    
-</span>
-<a href='#V'> ...show cron</a>
-<div id='V'>1 4 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh <br>
-1 4 * * * ${fr}configs/refs/heads/main/mm -o ${to}mm <br>
-5 4 * * * C_ID=V ${to}do.sh <br>
-</div></p>
 </div>
 
 	<div class="box">
 <p><i>US</i> rawte<b>x</b>t.club <span class="sml">45.33.66.185</span> 
 <a href="https://rawtext.club/">r.c</a> 
 <a href="https://rawtext.club/~identity2">r.c/~i72</a> </p>
-
-<p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]X rawtext.cl Lin                 sh <br>
-!X <s>w3m</s> lynx <s>jek</s> <s>nwsb</s>    <br>
-[X y2:x:1025:1025::/h/y2:/b/sh<br>
-(X 12:05/11 04:05/11 |dn.2   hk.    av.       <br>
-
-<span style='color: var(--col10-7);'>03:59   2:22 2:03    :30 2:03 8:51  :20 4:21 1:58 1:  :17   
-</span>
-<a href='#X'> ...show cron</a>
-<div id='X'>1 4 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh <br>
-1 4 * * * ${fr}configs/refs/heads/main/mm -o ${to}mm <br>
-5 4 * * * C_ID=X ${to}do.sh <br> 
-</div></p>
 </div>
 
 	<div class="box">
@@ -309,35 +149,12 @@ Svr: connect.tilde.tel, Port: 5060, Transp: UDP</p>
 <p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]S sfo sfo    Lin     7.4 3.1 3.3 sh <br>
 !S w3m lynx jek <s>nwsb</s>    <br>
 [S aa:x:1000:1000::/h/aa:/b/bash<br>
-(S 12:05/11 12:05/11 |dn.1   hk.    av.       <br>
-
-<span style='color: var(--col10-7);'>access list off for aaa</span>
-<a href='#S'> ...show cron</a>
-<div id='S'>1 12 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh <br>
-1 12 * * * ${fr}configs/refs/heads/main/mm -o ${to}mm <br>
-5 12 * * * C_ID=S ${to}do.sh <br>
-20 13 * * * ${to}gettrans.sh <br>
-</div></p>
+(S 12:05/11 12:05/11 |dn.1   hk.    av.  </p>
 </div>
 
 	<div class="bsd">
 <p><i>HK.</i> <span class="sml">    HP Mini 32bit</span> Mi<b>7</b>
 <span class="sml">sufbo.tplinkdns.com</span></p>
-
-<p><u> Hostname   Os  Lua Php Rb  Gem Sh </u><br>]Y mi         Fre                 bash<br>
-!Y <s>w3m</s> <s>lynx</s> <s>jek</s> <s>nwsb</s>    <br>
-[Y aa:*:1001:1001:aa:/h/aa:/u/l/b/bash<br>
-(Y 15:39/21 15:39/21 |dn.4   hk.    av.       <br>
-
-<span style='color: var(--col10-7);'> :33  13:45 :47 8595+18:30 8596+11:56   2:36   1:12 2:53 3:24 1701 2:05 1701   2:19   
-</span>
-<a href='#Y'> ...show cron</a>
-<div id='Y'>1 12 * * * ${fr}act/refs/heads/main/do.sh -o ${to}do.sh  <br>
-1 12 * * * ${fr}configs/refs/heads/main/mm -o ${to}mm <br>
-1 12 * * * ${fr}configs/refs/heads/main/urls -o <br>
-5 12 * * * C_ID=Y ${to}do.sh >/dev/null 2>&
-#*/30 * * * * /home/aaa/ip_monitor.sh <br>
-</div></p>
 </div>
 
 	<div class="box"> <p><i>Mob</i> <b>le</b><span class="sml">novo</p>
