bb0642fd1730432957737a9ef647123f9b049ae8
Kym Michael <kym.michael@gmail.com>
2025-08-15 21:08:38 +0800

    Update i.php

--- i.php
+++ i.php
@@ -3,6 +3,105 @@ $head = file_get_contents('https://raw.githubusercontent.com/sustance/sustance.g
 echo $head;
 ?>
 
+<div class="bsd">
+<p>
+<i>DE.</i> <b>b</b>sd.tilde.team <span class="sml">157.90.196.52</span> 
+<a href="https://bsd.tilde.team/">b.t.t</a> 
+<a href="https://bsd.tilde.team/~identity2">b.t.t/~i52</a> 
+</p>
+
+<i>US.</i> <b>c</b>trl-c.club <span class="tin">165.227.127.54</span> <i>NewsB</i> 
+<a href="https://ctrl-c.club/">c.c</a>
+<a href="https://ctrl-c.club/~i52">c.c/~i72</a>
+<a href="https://ctrl-c.club/~identity2/lineage/">c.c/~i72/lineage</a> &nbsp 
+</p>
+<p>
+<i>GB.</i> <b>d</b>imesion.sh <span class="sml">209.97.187.90</span> 
+<a href="https://dimension.sh/">d.s</a> 
+<a href="https://dimension.sh/~identity2">d.s/~i72</a></p>
+
+<p>
+<i>DE.</i> <b>e</b>nvs.net <span class="sml">89.163.145.170</span> <i>remi</i> 
+identity2@linkedin.com aaa John Smith p;;lin <a href="https://mail.envs.net/webmail/#?action=listmailbox">identity2@envs.net</a><br>
+<a href="https://envs.net/">e.n</a>  
+<a href="https://envs.net/~identity2">e.n/~i72</a>  
+<a href="https://envs.net/~identity2/lineage/">e.n/~i72/lineage</a> 
+</p>
+
+<p>
+<i>DE.</i> <b>f</b>reeshell.de <span class="sml">116.202.128.144</span> 
+<a href="https://freeshell.de/">f.d</a> 
+<a href="https://freeshell.de/~identity">f.d/~i6y</a> 
+<a href="https://freeshell.de/~identity/lineage/">f.d/~i6y/lineage</a> 
+SHA256:<br> pDFR0atCakVtmZProASRXRGq2aXyKxW6KMx8F6gZtvE (RSA)new 
+ssh ssh.freeshell.de -p 443 or -p 32768
+</p>
+
+<p>
+<i>??.</i>  tilde.<b>g</b>uru <span class="sml">95.179.178.246</span> 
+<a href="https://tilde.guru">t.g</a>  
+<a href="https://tilde.guru/~edi">t.g/~edi</a>
+</p>
+
+
+<p>
+<i>CA.</i>  t<b>h</b>unix.net <span class="sml">142.44.150.185</span> 
+<a href="https://thunix.net">t.n</a>  
+<a href="https://thunix.net/~id">t.n/~id</a> 
+2043108 <a href="https://tilde.tel">t.tel</a><br>
+Uname: 2043108, Pwrd: p;;tel,<br>
+Svr: connect.tilde.tel, Port: 5060, Transp: UDP
+</p>
+
+<p><i>DE.</i> tilde.<b>i</b>nstitute. <span class="sml">opt-svr</span>
+<span class="sml">159.69.146.152</span>  
+<a href="https://tilde.institute">t.i</a>  
+<a href="https://id2.tilde.institute">id2.t.i</a> </p>
+
+<p><i>DE.</i> pro<b>j</b>ectsegfau.lt <span class="sml">45.145.41.226</span> 
+<a href="https://projectsegfau.lt/">p.l</a> 
+<a href="https://identity2.p.projectsegfau.lt/">i.p.p.l/~i72</a></p>
+
+
+  <p><i>DE.</i> pr<b>o</b>jectsegfau.lt <span class="sml">45.145.41.226</span>
+<a href="https://projectsegfau.lt/">p.l</a> 
+<a href="https://id.p.projectsegfau.lt/">i.p.p.l/~id</a>
+  </p>
+  
+<p><i>CA.</i> &nbsp; tilde.<b>p</b>ink  
+<span class="sml">198.50.210.248 </span> <a href="https://tilde.pink/">t.p</a>  
+<a href="https://tilde.pink/~id2">t.p/~id2</a> 
+</p>
+
+<p><i>CA.</i> tilde.<b>t</b>eam <span class="sml">198.50.210.248</span> <i>Newsb</i> <a href="https://tilde.team/">t.t</a> 
+<a href="https://tilde.team/~identity2">t.t/~i72</a> 
+<a href="https://tilde.team/~identity2/lineage">t.t/~i72/lineage</a> 
+</p>
+
+<p><i>US.</i> <b>v</b>ern.cc <span class="sml">5.161.108.85</span> 
+<a href="https://vern.cc/">v.c</a> 
+<a href="https://id.vern.cc/">id.v.c/~id</a>
+</p>
+
+<p><i>US</i> rawte<b>x</b>t.club <span class="sml">45.33.66.185</span> 
+<a href="https://rawtext.club/">r.c</a> 
+<a href="https://rawtext.club/~identity2">r.c/~i72</a> 
+</p>
+
+<p><i>US.</i>    <b>s</b>fo     <span class="sml">104.245.33.223</span> 
+<a href="http://identity2.com">http i.c</a> 
+<a href="http://nix.identity2.com">http nix.i.c</a>
+</p>
+
+ <p><i>HK.</i> <span class="sml">    HP Mini 32bit</span> Mi<b>7</b>
+<span class="sml">sufbo.tplinkdns.com</span>
+</p> 
+  
+</div>
+
+
+
+
 <div class="box"> 
 <p><i>Mob</i> <b>le</b><span class="sml">novo</p>
 <p>SIP Address	2233589729@sip2sip.info<br>
