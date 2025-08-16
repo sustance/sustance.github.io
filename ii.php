33d998e7e7a24542d685bf72fb652437996b2f1b
Kym Michael <kym.michael@gmail.com>
2025-08-16 08:51:19 +0800

    Update ii.php

--- ii.php
+++ ii.php
@@ -3,7 +3,7 @@ $head = file_get_contents('https://raw.githubusercontent.com/sustance/sustance.g
 echo $head;
 ?>
 
-<div id="async-content-b bsd" class="async-content">
+<div id="async-content-b" class="async-content bsd">
 <p>
 <i>DE.</i> <b>b</b>sd.tilde.team <span class="sml">157.90.196.52</span> 
 <a href="https://bsd.tilde.team/">b.t.t</a> 
@@ -15,7 +15,7 @@ $b = file_get_contents('https://bsd.tilde.team/~identity2/a.txt'); echo $b;
 </div>  
 
 
-<div id="async-content-c bsd" class="async-content">
+<div id="async-content-c" class="async-content bsd">
 <i>US.</i> <b>c</b>trl-c.club <span class="tin">165.227.127.54</span> <i>NewsB</i> 
 <a href="https://ctrl-c.club/">c.c</a>
 <a href="https://ctrl-c.club/~i52">c.c/~i72</a>
@@ -25,7 +25,7 @@ $c = file_get_contents('https://ctrl-c.club/~identity2/a.txt'); echo $c;
 ?>
 </div>
 
-<div id="async-content-d bsd" class="async-content">
+<div id="async-content-d" class="async-content bsd">
 <p>
 <i>GB.</i> <b>d</b>imesion.sh <span class="sml">209.97.187.90</span> 
 <a href="https://dimension.sh/">d.s</a> 
@@ -35,7 +35,7 @@ $c = file_get_contents('https://ctrl-c.club/~identity2/a.txt'); echo $c;
 ?>
 </div>
 
-<div id="async-content-e bsd" class="async-content">
+<div id="async-content-e" class="async-content bsd">
 <p>
 <i>DE.</i> <b>e</b>nvs.net <span class="sml">89.163.145.170</span> <i>remi</i> 
 identity2@linkedin.com aaa John Smith p;;lin <a href="https://mail.envs.net/webmail/#?action=listmailbox">identity2@envs.net</a><br>
@@ -47,7 +47,7 @@ identity2@linkedin.com aaa John Smith p;;lin <a href="https://mail.envs.net/webm
 ?>
 </div>
 
-<div id="async-content-f bsd" class="async-content">
+<div id="async-content-f" class="async-content bsd">
 <p>
 <i>DE.</i> <b>f</b>reeshell.de <span class="sml">116.202.128.144</span> 
 <a href="https://freeshell.de/">f.d</a> 
@@ -60,7 +60,7 @@ $f = file_get_contents('https://freeshell.de/~identity/a.txt'); echo $f;
 ?>
 </div>
 
-<div id="async-content-g bsd" class="async-content">
+<div id="async-content-g" class="async-content bsd">
 <p>
 <i>??.</i>  tilde.<b>g</b>uru <span class="sml">95.179.178.246</span> 
 <a href="https://tilde.guru">t.g</a>  
@@ -71,7 +71,7 @@ $f = file_get_contents('https://freeshell.de/~identity/a.txt'); echo $f;
 ?>
 </div>
 
-<div id="async-content-h bsd" class="async-content">
+<div id="async-content-h" class="async-content bsd">
 <p>
 <i>CA.</i>  t<b>h</b>unix.net <span class="sml">142.44.150.185</span> 
 <a href="https://thunix.net">t.n</a>  
@@ -84,7 +84,7 @@ Svr: connect.tilde.tel, Port: 5060, Transp: UDP</p>
 ?>
 </div>
 
-<div id="async-content-i bsd" class="async-content">
+<div id="async-content-i" class="async-content bsd">
 <p><i>DE.</i> tilde.<b>i</b>nstitute. <span class="sml">opt-svr</span>
 <span class="sml">159.69.146.152</span>  
 <a href="https://tilde.institute">t.i</a>  
@@ -94,7 +94,7 @@ Svr: connect.tilde.tel, Port: 5060, Transp: UDP</p>
 ?>
 </div>
 
-<div id="async-content-j bsd" class="async-content">
+<div id="async-content-j" class="async-content bsd">
 <p><i>DE.</i> pro<b>j</b>ectsegfau.lt <span class="sml">45.145.41.226</span> 
 <a href="https://projectsegfau.lt/">p.l</a> 
 <a href="https://identity2.p.projectsegfau.lt/">i.p.p.l/~i72</a></p>
@@ -103,7 +103,7 @@ $j = file_get_contents('https://identity2.p.projectsegfau.lt/a.txt'); echo $j;
 ?>
 </div>
 
-<div id="async-content-o bsd" class="async-content">
+<div id="async-content-o" class="async-content bsd">
 <p><i>DE.</i> pr<b>o</b>jectsegfau.lt <span class="sml">45.145.41.226</span>
 <a href="https://projectsegfau.lt/">p.l</a> 
 <a href="https://id.p.projectsegfau.lt/">i.p.p.l/~id</a></p>
@@ -112,7 +112,7 @@ $o = file_get_contents('https://id.p.projectsegfau.lt/a.txt'); echo $o;
 ?>	
 </div>
 
-<div id="async-content-p bsd" class="async-content">
+<div id="async-content-p" class="async-content bsd">
 <p><i>CA.</i> &nbsp; tilde.<b>p</b>ink  
 <span class="sml">198.50.210.248 </span> <a href="https://tilde.pink/">t.p</a>  
 <a href="https://tilde.pink/~id2">t.p/~id2</a> </p>
@@ -121,7 +121,7 @@ $o = file_get_contents('https://id.p.projectsegfau.lt/a.txt'); echo $o;
 ?>
 </div>
 
-<div id="async-content-t bsd" class="async-content">
+<div id="async-content-t " class="async-content bsd">
 <p><i>CA.</i> tilde.<b>t</b>eam <span class="sml">198.50.210.248</span> <i>Newsb</i> <a href="https://tilde.team/">t.t</a> 
 <a href="https://tilde.team/~identity2">t.t/~i72</a> 
 <a href="https://tilde.team/~identity2/lineage">t.t/~i72/lineage</a> </p>
@@ -130,7 +130,7 @@ $o = file_get_contents('https://id.p.projectsegfau.lt/a.txt'); echo $o;
 ?>
 </div>
 
-<div id="async-content-v bsd" class="async-content">
+<div id="async-content-v " class="async-content bsd">
 <p><i>US.</i> <b>v</b>ern.cc <span class="sml">5.161.108.85</span> 
 <a href="https://vern.cc/">v.c</a> 
 <a href="https://id.vern.cc/">id.v.c/~id</a></p>
@@ -139,7 +139,7 @@ $o = file_get_contents('https://id.p.projectsegfau.lt/a.txt'); echo $o;
 ?> 
 </div>
 
-<div id="async-content-x bsd" class="async-content">
+<div id="async-content-x " class="async-content bsd">
 <p><i>US</i> rawte<b>x</b>t.club <span class="sml">45.33.66.185</span> 
 <a href="https://rawtext.club/">r.c</a> 
 <a href="https://rawtext.club/~identity2">r.c/~i72</a> </p>
@@ -148,7 +148,7 @@ $x = file_get_contents('https://thunix.net/~id/a-x.txt'); echo $x;
 ?>
 </div>
 
-<div id="async-content-s bsd" class="async-content">
+<div id="async-content-s " class="async-content bsd">
 <p><i>US.</i>    <b>s</b>fo     <span class="sml">104.245.33.223</span> 
 <a href="http://identity2.com">http i.c</a> 
 <a href="http://nix.identity2.com">http nix.i.c</a></p>
@@ -157,7 +157,7 @@ $x = file_get_contents('https://thunix.net/~id/a-x.txt'); echo $x;
 ?>
 </div>
 
-<div id="async-content-7 bsd" class="async-content">
+<div id="async-content-7 " class="async-content bsd">
 <p><i>HK.</i> <span class="sml">    HP Mini 32bit</span> Mi<b>7</b>
 <span class="sml">sufbo.tplinkdns.com</span>
 </p>
