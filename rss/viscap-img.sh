a4b9783c4fa23a04f9a0b71cd3cd2486ef376666
Kym Michael <kym.michael@gmail.com>
2026-06-28 07:55:53 +0800

    Create viscap-img.sh

---
+++ rss/viscap-img.sh
@@ -0,0 +1,7 @@
+
+curl -L \
+  -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36" \
+  -H "Accept: image/webp,image/apng,image/*,*/*;q=0.8" \
+  -H "Referer: https://www.visualcapitalist.com/" \
+  -o PeaceIndex-web.webp \
+  "https://www.visualcapitalist.com/wp-content/uploads/2026/06/PeaceIndex-web.webp"
